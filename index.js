388ec86569ba9f722637b040db33b50fe35f4ee0
Tim <tim.a.ung.12@gmail.com>
2024-02-12 18:28:08 +1100

    fetch db info and display it

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 import 'dotenv/config';
 import express from "express";
+import pg from "pg";
 
-const app = express()
-const port = 3000
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "throwing speeds",
+    password: process.env.PG_PASSWORD,
+    port: 5432
+});
 
-app.use(express.static("public"))
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+async function fetchUsers() {
+    const users = await db.query("SELECT * FROM users")
+    return users.rows
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const users = await fetchUsers() 
+        res.render("main.ejs", {users: users})
+    } catch (error) {
+        console.error('Error fetching data', error);
+        res.status(500).send('Error fetching data');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
