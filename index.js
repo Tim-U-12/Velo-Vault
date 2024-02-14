3e7ccc04260ce132d58062e3a73eec620242e773
Tim <tim.a.ung.12@gmail.com>
2024-02-14 15:21:46 +1100

    fix syntax error when importing pg

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 import express from "express";
-import {Pool} from "pg";
+import pg from "pg";
 
+const { Pool } = pg;
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,19 +17,18 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs'); // Ensure EJS is set as the view engine
 
 async function fetchUsers() {
-    const users = await db.query("SELECT * FROM users")
+    const users = await pool.query("SELECT * FROM users")
     return users.rows
 }
 
 app.get('/', async (req, res) => {
-    res.render("index.ejs")
-    // try {
-    //     const users = await fetchUsers() 
-    //     res.render("main.ejs", {users: users})
-    // } catch (error) {
-    //     console.error('Error fetching data', error);
-    //     res.status(500).send('Error fetching data');
-    // }
+    try {
+        const users = await fetchUsers() 
+        res.render("main.ejs", {users: users})
+    } catch (error) {
+        console.error('Error fetching data', error);
+        res.status(500).send('Error fetching data');
+    }
 });
 
 app.listen(port, () => {
