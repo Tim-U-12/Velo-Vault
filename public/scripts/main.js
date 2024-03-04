bf0fb47d8afc031d1de4b34bd0070bbd1a9bb1ff
Tim <tim.a.ung.12@gmail.com>
2024-03-04 20:16:54 +1100

    replace eventlistener with onclick

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const genderButtons = document.querySelectorAll('button[data-gender]')
-    genderButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            const gender = this.getAttribute('data-gender');
-            fetchResults(gender);
-        });
-    });
-});
-
 function fetchResults(gender) {
     const url = `/filter?genderChoice=${gender}`;
-
     fetch(url)
-        .then(response => response.text()) // Get the response text (HTML)
+        .then(response => response.text())
         .then(html => {
-            // Insert the HTML directly into the results div
-            document.getElementById('results').innerHTML = html;
-            console.log("hello world")
+            document.getElementById('ladder-results').innerHTML = html;
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
