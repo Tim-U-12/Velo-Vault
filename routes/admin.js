12568cb3ea99b420abd69da031f11abfadcafc22
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-11 14:51:10 +1100

    move all admin routes to separate file

---
+++ routes/admin.js
@@ -0,0 +1,59 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/admin', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('admin.ejs')
+    } else {
+        res.redirect('/login')
+    }
+})
+
+router.get('/admin-create-user', (req, res) => {
+    res.render('./admin/create-user.ejs')
+})
+
+router.post('/admin-create-user', async (req, res) => {
+    const text = 'INSERT INTO users(first_name, last_name, dominant_arm, height_metres, sex) VALUES($1, $2, $3, $4, $5) RETURNING *';
+    const values = [
+        req.body.first_name,
+        req.body.last_name,
+        req.body.dominant_arm,
+        req.body.height_metres,
+        req.body.sex
+    ];
+
+    try {
+        const result = await pool.query(text, values);
+        console.log(result.rows[0])
+        res.redirect('/admin-create-user')
+    } catch (error) {
+        console.error(err);
+        res.status(500).send('Server error')
+    }
+});
+
+router.get('/admin-insert-throw', (req, res) => {
+    res.render('./admin/insert-throw.ejs')
+})
+
+router.post('/admin-insert-throw', async(req, res) => {
+    const text = 'INSERT INTO throws(id, date, throw_speed_kmh) VALUES ($1, $2, $3) RETURNING *'
+    const currentDate = new Date().toISOString().split('T')[0];
+    const values = [
+        req.body.id,
+        currentDate,
+        req.body.throw_speed,
+    ];
+    
+    try {
+        const result = await pool.query(text, values);
+        console.log(result.rows[0])
+        res.redirect('/admin-insert-throw')
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error')
+    }
+});
+
+export default router;
