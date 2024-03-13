acbf97a9b39cc8bd3e0b3488671a4e524f92c524
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-13 15:34:28 +1100

    Clean up

--- routes/admin.js
+++ routes/admin.js
@@ -10,68 +10,68 @@ router.get('/admin', (req, res) => {
     }
 })
 
-router.get('/admin-create-user', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render('./admin/create-user.ejs')
-    } else {
-        res.redirect('/login')
-    }
-})
-
-router.post('/admin-create-user', async (req, res) => {
-    if (req.isAuthenticated()) {
-        const text = 'INSERT INTO users(first_name, last_name, height, dominant_arm, sex) VALUES($1, $2, $3, $4, $5) RETURNING *';
-        const values = [
-            req.body.first_name,
-            req.body.last_name,
-            req.body.height,
-            req.body.dominant_arm,
-            req.body.sex
-        ];
-    
-        try {
-            const result = await pool.query(text, values);
-            console.log(result.rows[0])
-            res.redirect('/admin-create-user')
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Server error')
+router.route('/admin-create-user')
+    .get((req,res) => {
+        if (req.isAuthenticated()) {
+            res.render('./admin/create-user.ejs')
+        } else {
+            res.redirect('/login')
         }
-    } else {
-        res.redirect('/login')
-    }
-});
-
-router.get('/admin-insert-throw', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render('./admin/insert-throw.ejs')
-    } else {
-        res.redirect('/login')
-    }
-    
-})
-
-router.post('/admin-insert-throw', async(req, res) => {
-    if (req.isAuthenticated()) {
-        const text = 'INSERT INTO throws(user_id, throw_date, throw_type, throw_speed) VALUES ($1, $2, $3, $4) RETURNING *'
-        const currentDate = new Date().toISOString().split('T')[0];
-        const values = [
-            req.body.user_id,
-            currentDate,
-            req.body.throw_type,
-            req.body.throw_speed,
-        ];
+    })
+    .post(async (req, res) => {
+        if (req.isAuthenticated()) {
+            const text = 'INSERT INTO users(first_name, last_name, height, dominant_arm, sex) VALUES($1, $2, $3, $4, $5) RETURNING *';
+            const values = [
+                req.body.first_name,
+                req.body.last_name,
+                req.body.height,
+                req.body.dominant_arm,
+                req.body.sex
+            ];
         
-        try {
-            const result = await pool.query(text, values);
-            res.redirect('/admin-insert-throw')
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Server error')
+            try {
+                const result = await pool.query(text, values);
+                console.log(result.rows[0])
+                res.redirect('/admin-create-user')
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Server error')
+            }
+        } else {
+            res.redirect('/login')
         }
-    } else {
-        res.redirect('/login')
-    }
-});
+    })
+
+router.route('/admin-insert-throw')
+    .get((req, res) => {
+        if (req.isAuthenticated()) {
+            res.render('./admin/insert-throw.ejs')
+        } else {
+            res.redirect('/login')
+        }
+        
+    })
+    .post(async (req, res) => {
+        if (req.isAuthenticated()) {
+            const text = 'INSERT INTO throws(user_id, throw_date, throw_type, throw_speed) VALUES ($1, $2, $3, $4) RETURNING *'
+            const currentDate = new Date().toISOString().split('T')[0];
+            const values = [
+                req.body.user_id,
+                currentDate,
+                req.body.throw_type,
+                req.body.throw_speed,
+            ];
+            
+            try {
+                const result = await pool.query(text, values);
+                res.redirect('/admin-insert-throw')
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Server error')
+            }
+        } else {
+            res.redirect('/login')
+        }
+    })
 
 export default router;
